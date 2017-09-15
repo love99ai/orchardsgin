1f6a4ae606807f93b50e95ce5794ad8ee7e96eb8
love99ai <529453873@qq.com>
2017-09-15 18:09:04 +0800

    角色管理

--- src/main/resources/mybatis/orchard_sign.sql
+++ src/main/resources/mybatis/orchard_sign.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-09-14 18:03:00
+Date: 2017-09-15 18:08:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,7 +26,7 @@ CREATE TABLE `bt_right` (
   `parentId` int(10) NOT NULL DEFAULT '0' COMMENT '父ID',
   `indexs` int(11) DEFAULT NULL COMMENT '排序标示',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of bt_right
@@ -39,6 +39,8 @@ INSERT INTO `bt_right` VALUES ('-2', '商品管理', null, '商品管理', '0', 
 INSERT INTO `bt_right` VALUES ('-1', '用户管理', null, '用户管理', '0', '1');
 INSERT INTO `bt_right` VALUES ('1', '商户管理', '/admin/business', '商户管理', '-1', '1');
 INSERT INTO `bt_right` VALUES ('2', '用户管理', null, '用户管理', '-1', '2');
+INSERT INTO `bt_right` VALUES ('3', '角色', '/admin/groupList', '角色管理', '-6', '1');
+INSERT INTO `bt_right` VALUES ('4', '管理员', null, '管理员管理', '-6', '2');
 
 -- ----------------------------
 -- Table structure for `bt_role_rights`
@@ -53,7 +55,7 @@ CREATE TABLE `bt_role_rights` (
   KEY `role_id` (`roleId`),
   CONSTRAINT `rights_id` FOREIGN KEY (`rightsId`) REFERENCES `bt_right` (`id`),
   CONSTRAINT `role_id` FOREIGN KEY (`roleId`) REFERENCES `tb_admin_group` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of bt_role_rights
@@ -66,6 +68,8 @@ INSERT INTO `bt_role_rights` VALUES ('5', '-5', '1');
 INSERT INTO `bt_role_rights` VALUES ('6', '-6', '1');
 INSERT INTO `bt_role_rights` VALUES ('7', '1', '1');
 INSERT INTO `bt_role_rights` VALUES ('8', '2', '1');
+INSERT INTO `bt_role_rights` VALUES ('9', '3', '1');
+INSERT INTO `bt_role_rights` VALUES ('10', '4', '1');
 
 -- ----------------------------
 -- Table structure for `tb_admin`
@@ -73,7 +77,7 @@ INSERT INTO `bt_role_rights` VALUES ('8', '2', '1');
 DROP TABLE IF EXISTS `tb_admin`;
 CREATE TABLE `tb_admin` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `permission` int(10) NOT NULL COMMENT '权限范围（组）',
+  `permission` int(10) DEFAULT NULL COMMENT '权限范围（组）',
   `nickname` varchar(20) NOT NULL COMMENT '昵称',
   `uName` varchar(20) NOT NULL COMMENT '登录用户名',
   `uPwd` varchar(18) NOT NULL COMMENT '登录密码',
@@ -84,15 +88,15 @@ CREATE TABLE `tb_admin` (
   `phone` varchar(15) DEFAULT NULL COMMENT '电话',
   PRIMARY KEY (`id`),
   UNIQUE KEY `u_name` (`uName`),
-  KEY `admin_group` (`permission`),
-  CONSTRAINT `admin_group` FOREIGN KEY (`permission`) REFERENCES `tb_admin_group` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  KEY `admin_group` (`permission`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_admin
 -- ----------------------------
-INSERT INTO `tb_admin` VALUES ('1', '1', '总管', 'admin', '123456', '2017-09-05 00:00:00', '1', '2017-09-07 14:27:48', '', null);
+INSERT INTO `tb_admin` VALUES ('1', '1', '管理员', 'admin', '123456', '2017-09-15 17:53:49', '0', null, null, null);
 INSERT INTO `tb_admin` VALUES ('2', '2', '万达12', 'shanghu', '', '2017-09-07 14:30:44', '1', null, null, '15826183882');
+INSERT INTO `tb_admin` VALUES ('4', '3', '大柯米', 'dakemi', '111111', '2017-09-15 10:00:09', '1', null, null, '19999999999');
 
 -- ----------------------------
 -- Table structure for `tb_admin_group`
@@ -104,13 +108,14 @@ CREATE TABLE `tb_admin_group` (
   `groupMsg` varchar(150) DEFAULT NULL COMMENT '权限组描述',
   PRIMARY KEY (`id`),
   UNIQUE KEY `group_name` (`groupName`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_admin_group
 -- ----------------------------
-INSERT INTO `tb_admin_group` VALUES ('1', '超级管理员', null);
+INSERT INTO `tb_admin_group` VALUES ('1', '超级管理员', '王一般的存在');
 INSERT INTO `tb_admin_group` VALUES ('2', '商家', null);
+INSERT INTO `tb_admin_group` VALUES ('4', '收银员', '平台资金管理');
 
 -- ----------------------------
 -- View structure for `v_admin`
